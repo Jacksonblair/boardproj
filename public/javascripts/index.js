adb22df1f4c833bb1ba35ec6077b88afab645ac9
Jacksonblair <jackson.blair@live.com>
2019-11-10 13:40:24 +1100

    adjust frontend js for mobile

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -120,151 +120,152 @@ $(document).ready(function(){
 		updateBoardList();
 	});
 
-});
 
-function pinPosts() {
-	boardState.action.type = "pin";
-	updateBoard();
-}
-
-function searchformsubmit() {
-	console.log('wot');
-	var search = $('#searchinput').val();
-	boardState.filters.search = search.toLowerCase();
-	updateBoardFilters();
-}
-
-/* CONTENT VIEW FUNCTIONALITY */
-
-function showSiblings(element) {
-	var element = $(element);
-	if (element.attr('value') == "hidden") {
-		element.attr('value', 'shown');
-		$(element).siblings('.post-segment-container').css('display', 'inline-block');
-		$(element).siblings('.post-functionality-container').css('visibility', 'visible');
-	} else {
-		element.attr('value', 'hidden');
-		$(element).siblings('.post-segment-container').css('display', 'none');
-		$(element).siblings('.post-functionality-container').css('visibility', 'hidden');
+	function pinPosts() {
+		boardState.action.type = "pin";
+		updateBoard();
 	}
-}
 
-function showContent(post) {
-	// document.getElementById('contentviewerpostdeletebutton').attributes.action = `./${post.id}?_method=DELETE`;
-	console.log(document.getElementById('postcontentdeleteform').attributes.action.value = `./${post.id}?_method=DELETE`);
-	console.log(document.getElementById('postcontenteditbutton').attributes.href.value = `./${post.id}/edit_post`);
-	console.log(document.getElementById('mobilepostcontentdeleteform').attributes.action.value = `./${post.id}?_method=DELETE`);
-	console.log(document.getElementById('mobilepostcontenteditbutton').attributes.href.value = `./${post.id}/edit_post`);
-	// .ui.header.content-viewer-post-description
-}
+	function searchformsubmit() {
+		console.log('wot');
+		var search = $('#searchinput').val();
+		boardState.filters.search = search.toLowerCase();
+		updateBoardFilters();
+	}
 
-/* BOARD FILTER FUNCTIIONALITY */
+	/* CONTENT VIEW FUNCTIONALITY */
 
-var postToViewIndex;
-
-var boardState = {
-	filters: {
-		category: "",
-		dateSetting: 'ALL',
-		date: "",
-		enddate: "",
-		startdate: "",
-		search: ""
-	},
-	action: {
-		type: "",
-		checked: [],
-		resetAction: function() {
-			this.type = "",
-			this.checked = [];
-			return;
+	function showSiblings(element) {
+		var element = $(element);
+		if (element.attr('value') == "hidden") {
+			element.attr('value', 'shown');
+			$(element).siblings('.post-segment-container').css('display', 'inline-block');
+			$(element).siblings('.post-functionality-container').css('visibility', 'visible');
+		} else {
+			element.attr('value', 'hidden');
+			$(element).siblings('.post-segment-container').css('display', 'none');
+			$(element).siblings('.post-functionality-container').css('visibility', 'hidden');
 		}
 	}
-};
 
-function updateBoardList() {
-	console.log('updating list of avail. boards');
-    $.ajax({
-        type: 'post',
-        url: '/board/update_boardlist'
-    })
-    .then(function(data) {
-    	console.log(data);
-    	$('#boardlistmenu').html(data)
-  //   	$('.ui.dropdown')
-		// .dropdown();
-    });
-}
+	function showContent(post) {
+		// document.getElementById('contentviewerpostdeletebutton').attributes.action = `./${post.id}?_method=DELETE`;
+		console.log(document.getElementById('postcontentdeleteform').attributes.action.value = `./${post.id}?_method=DELETE`);
+		console.log(document.getElementById('postcontenteditbutton').attributes.href.value = `./${post.id}/edit_post`);
+		console.log(document.getElementById('mobilepostcontentdeleteform').attributes.action.value = `./${post.id}?_method=DELETE`);
+		console.log(document.getElementById('mobilepostcontenteditbutton').attributes.href.value = `./${post.id}/edit_post`);
+		// .ui.header.content-viewer-post-description
+	}
 
-/* Ajax request, called for each change to a filter element */
-function updateContentViewer(post_id) {
-	console.log('updating post content');
-    $.ajax({
-        type: 'post',
-        url: './update_content',
-        data: JSON.stringify({ post_id: post_id }),
-        contentType: 'application/json'
-    })
-    .then(function(data) {
-    	if (data) {
-    		console.log(data);
-	    	$('#contentcontainer').html(data);
-	    	$('#mobilecontentcontainer').html(data);
-	    	return;
-    	}
-    })
-}
+	/* BOARD FILTER FUNCTIIONALITY */
 
-function updateBoard() {
-	console.log('updating board');
-    $.ajax({
-        type: 'post',
-        url: './update_board',
-        data: JSON.stringify(boardState.action),
-        contentType: 'application/json'
-    })
-    .then(function(data){
-    	if (data) {
+	var postToViewIndex;
 
-			// Reset boardstate
-			boardState.action.resetAction();
+	var boardState = {
+		filters: {
+			category: "",
+			dateSetting: 'ALL',
+			date: "",
+			enddate: "",
+			startdate: "",
+			search: ""
+		},
+		action: {
+			type: "",
+			checked: [],
+			resetAction: function() {
+				this.type = "",
+				this.checked = [];
+				return;
+			}
+		}
+	};
 
-        	$('#feedcolumn').html(data);
-        	// console.log(data);
-			$('.ui.checkbox').checkbox({
-			    onChecked: function() {
-			    	boardState.action.checked.push($(this).attr('value'));
-			       	console.log(boardState.action.checked);
-			    },
-			    onUnchecked: function() {
-			       	boardState.action.checked.splice(boardState.action.checked.indexOf($(this).attr('value')), 1);
-			       	console.log(boardState.action.checked);
-			    }
-			});
-			return;
-    	}
-    });
-}
+	function updateBoardList() {
+		console.log('updating list of avail. boards');
+	    $.ajax({
+	        type: 'post',
+	        url: '/board/update_boardlist'
+	    })
+	    .then(function(data) {
+	    	console.log(data);
+	    	$('#boardlistmenu').html(data)
+	  //   	$('.ui.dropdown')
+			// .dropdown();
+	    });
+	}
 
-function updateBoardFilters() {
+	/* Ajax request, called for each change to a filter element */
+	function updateContentViewer(post_id) {
+		console.log('updating post content');
+	    $.ajax({
+	        type: 'post',
+	        url: './update_content',
+	        data: JSON.stringify({ post_id: post_id }),
+	        contentType: 'application/json'
+	    })
+	    .then(function(data) {
+	    	if (data) {
+	    		console.log(data);
+		    	$('#contentcontainer').html(data);
+		    	$('#mobilecontentcontainer').html(data);
+		    	return;
+	    	}
+	    })
+	}
 
-	console.log('updating filters...')
-	console.log(boardState.filters)
+	function updateBoard() {
+		console.log('updating board');
+	    $.ajax({
+	        type: 'post',
+	        url: './update_board',
+	        data: JSON.stringify(boardState.action),
+	        contentType: 'application/json'
+	    })
+	    .then(function(data){
+	    	if (data) {
 
-    $.ajax({
-        type: 'post',
-        url: './update_filters',
-        data: JSON.stringify(boardState.filters),
-        contentType: 'application/json'
-    })
-    .then(function(data){
-    	if (data) {
-    		// console.log(data);
-        	$('#feedcolumn').html(data);
-        	// console.log(data);
-    	} else {
-			console.log('wot');  		
-    	}
-    });
+				// Reset boardstate
+				boardState.action.resetAction();
 
-}
+	        	$('#feedcolumn').html(data);
+	        	// console.log(data);
+				$('.ui.checkbox').checkbox({
+				    onChecked: function() {
+				    	boardState.action.checked.push($(this).attr('value'));
+				       	console.log(boardState.action.checked);
+				    },
+				    onUnchecked: function() {
+				       	boardState.action.checked.splice(boardState.action.checked.indexOf($(this).attr('value')), 1);
+				       	console.log(boardState.action.checked);
+				    }
+				});
+				return;
+	    	}
+	    });
+	}
+
+	function updateBoardFilters() {
+
+		console.log('updating filters...')
+		console.log(boardState.filters)
+
+	    $.ajax({
+	        type: 'post',
+	        url: './update_filters',
+	        data: JSON.stringify(boardState.filters),
+	        contentType: 'application/json'
+	    })
+	    .then(function(data){
+	    	if (data) {
+	    		// console.log(data);
+	        	$('#feedcolumn').html(data);
+	        	// console.log(data);
+	    	} else {
+				console.log('wot');  		
+	    	}
+	    });
+
+	}
+
+});
